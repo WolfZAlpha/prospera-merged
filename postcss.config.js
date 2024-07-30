6e65f0dda5ea71efeed1a1c166495b530998bacb
WolfZAlpha <ayowolfz@proton.me>
2024-07-30 13:44:17 -0400

    test for deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      // Ignore the color-adjust deprecation warning
+      ignoreWarnings: [{ rule: 'color-adjust' }],
     },
-  }
-  
+    'postcss-replace': {
+      pattern: /color-adjust/g,
+      data: { replace: 'print-color-adjust' }
+    },
+  },
+}
