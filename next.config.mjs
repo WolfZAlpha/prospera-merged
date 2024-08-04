df38fa342afa85b53abb440e52e9b9c2a7c72d3b
WolfZAlpha <ayowolfz@proton.me>
2024-08-03 21:48:15 -0400

    www

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.prosperaico.com',
+          },
+        ],
+        destination: 'https://prosperaico.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
